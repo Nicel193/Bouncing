19e091107109e603d26559f171bc1089a5724509
Nicel193 <nicelgoog@gmail.com>
2023-10-28 01:52:53 +0300

    Try realize beats generation

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -8,37 +8,51 @@ namespace Map
 {
     public class MapGenerator : MonoBehaviour
     {
+        [SerializeField] private AudioSource _musicAudio;
         [SerializeField] private Transform _player;
         [SerializeField] private PlayerConfig _playerConfig;
         [SerializeField] private ChunkPlacer _chunkPlacer;
         [SerializeField] private string _audioFilePath = "./Assets/test2.mp3";
         
         private IBeatDetector _beatDetector;
+        List<double> _beatTimes = new List<double>();
+        
+        private int _lastBeatIndex;
+        private double _timeLoadOffset = 30f;
+        private double _timePart;
         
         private void Start()
         {
             _chunkPlacer.Init(_player);
-            
-            IBeatDetector beatDetector = new BeatDetectorBase();
-            List<double> beatTimes = beatDetector.DetectBeats(_audioFilePath);
-            
-            for (int i = 0; i < beatTimes.Count; i++)
-            {
-                Debug.Log(beatTimes[i]);
-                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                //S = V * T
-                sphere.transform.position = new Vector3(((float)beatTimes[i] * _playerConfig.VerticalPlayerSpeed), 0, _player.position.z);
-            }
+            _beatDetector = new BeatDetectorBase();
+            _beatTimes = _beatDetector.DetectBeats(_audioFilePath);
         }
 
         private void Update()
         {
             _chunkPlacer.Update();
+            
+            if(_musicAudio.time >= _timePart - _timeLoadOffset) LoadBeats();
         }
 
         private void LoadBeats()
         {
+            _timePart += _timeLoadOffset;
             
+            for (int i = _lastBeatIndex; i < _beatTimes.Count; i++)
+            {
+                if ((float) _beatTimes[i] <= _timePart)
+                {
+                    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    //S = V * T
+                    sphere.transform.position = new Vector3((float) _beatTimes[i] * _playerConfig.VerticalPlayerSpeed,
+                        0, _player.position.z);
+
+                    _lastBeatIndex = i;
+                    
+                    break;
+                }
+            }
         }
     }
 }
